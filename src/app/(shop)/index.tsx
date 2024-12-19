02bbb315f5d186233abe2fdcec776eef9a5c5ebc
Raymond9734 <rymadara97@gmail.com>
2024-12-19 20:01:34 +0300

    Implement product listing with FlatList

--- src/app/(shop)/index.tsx
+++ src/app/(shop)/index.tsx
@@ -1,14 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View ,FlatList} from 'react-native'
 import React from 'react'
-
+import { PRODUCTS } from '../../../assets/products'
+import ProductList from '../../components/product-list'
+import { ListHeader } from '../../components/list-header'
 const index = () => {
   return (
     <View>
-      <Text>index</Text>
+     <FlatList
+     data={PRODUCTS}
+        renderItem={({ item }) =>
+         <ProductList products={item}/>
+        }
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={2}
+        ListHeaderComponent={ListHeader}
+        contentContainerStyle={styles.flatlistContent}
+        columnWrapperStyle={styles.flatlistcolumn}
+        style={{paddingHorizontal: 10,paddingVertical:5}}
+      />
     </View>
   )
 }
 
 export default index
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  flatlistContent:{
+    paddingBottom: 20,
+  },
+  flatlistcolumn:{
+    justifyContent: 'space-between',
+  }
+})
